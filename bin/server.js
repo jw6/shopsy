8cc99c60cbf6c5ef5c634c8a72c18076b0764233
Jay Wang <jaywang007@yahoo.com>
2018-02-28 22:05:59 -0600

    finished server

--- bin/server.js
+++ bin/server.js
@@ -17,3 +17,38 @@ mongoose.connect(config.mongodb.dsn)
 const app = App(config);
 const port = process.env.PORT || '3000';
 app.set('port', port);
+
+function onError(error) {
+  if (error.syscall != 'listen') {
+    throw err;
+  }
+
+  const bind = typeof port = 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`
+
+  // Handle specific listen errors with friendly messages
+  if (error.code === 'EACCES') {
+    log.fatal(`${bind} requires elevated privileges`);
+    process.exit(1);
+  } else if (error.code === 'EADDRINUSE') {
+    log.fatal(`${bind} is already in use`);
+    process.exit(1);
+  } else {
+     throw err;
+  }
+}
+
+const server = http.createServer(app);
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+  ? `pipe ${addr}`
+  : `port ${addr.port}`
+
+  log.info(`${config.applicationName} listening on ${bind}` );
+}
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
