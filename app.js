8524d55d55296be91b9a4b46d696f1844ab597f9
Jay Wang <jaywang007@yahoo.com>
2018-02-28 23:23:24 -0600

    set up session

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const routeHandler = require('./routes');
+
+module.exports = (config) => {
+  const app = express();
+
+  // static folder for view
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // parse incoming request body
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: fasle }));
+
+  app.set('trsut proxy', 1);
+  app.use(session({
+    secret: 'very secret to encrypt session',
+    resave: false,
+    saveUninitialized: false,
+  }));
+}
